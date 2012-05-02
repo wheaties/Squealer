c2bf41646146bfe262bb2929f139c91c40bc3b93
Wheaties <owreese@gmail.com>
2012-05-01 21:51:46 -0400

    tests passing except the one that matters...

--- src/test/scala/com/wheaties/squealer/CoalesceSQLSpec.scala
+++ src/test/scala/com/wheaties/squealer/CoalesceSQLSpec.scala
@@ -10,13 +10,60 @@ class CoalesceSQLSpec {
 class CoalesceFromClauseSpec extends Specification{
   "substitute" should{
     val mapped = Map(Term("foo", None) -> Table("foo", None, Nil), Term("bar", Some("b")) -> Table("bar", None, Nil))
-    "substitute a table with a matching name" in{
-      val output = CoalesceFromClause.substitute("foo", mapped, _.copy(columns = List(ColumnDef("yo", "Int", None, None))))
-      val column = for{
+    val column = ColumnDef("yo", "Int", None, None)
+    def makeCol(name: String) ={
+      val output = CoalesceFromClause.substitute(name, mapped, _.copy(columns = List(column)))
+      for{
         (_, table) <- output
         singleCol <- table.columns
       } yield singleCol
-      column must contain(ColumnDef("yo", "Int", None, None))
+    }
+    "substitute a table with a matching name" in{
+      makeCol("foo") must contain(column)
+    }
+
+    "substitute a table with a matching aliased name" in{
+      makeCol("b") must contain(column)
+    }
+
+    "substitute nothing if no matching name exists" in{
+      makeCol("oh no") must not contain(column)
+    }
+  }
+
+  "nullColumns" in{
+    val table = Table("foo", None, ColumnDef("bar", "Int", None, None) :: Nil)
+    val nulled = CoalesceFromClause.nullColumns(table)
+
+    nulled.columns must contain(NullableColumnDef("bar", "Int", None))
+  }
+
+  "extractTerms" should{
+    val term = Term("foo", None)
+
+    "work with Term" in{
+      val out = CoalesceFromClause.extractTerms(term :: Nil)
+      out must contain(term)
+    }
+
+    "work with LeftJoin" in{
+      val out = CoalesceFromClause.extractTerms(LeftJoin(term, term) :: Nil)
+      out must contain(term)
+    }
+
+    "work with RightJoin" in{
+      val out = CoalesceFromClause.extractTerms(RightJoin(term, term) :: Nil)
+      out must contain(term)
+    }
+
+    "work with Join" in{
+      val out = CoalesceFromClause.extractTerms(Join(term, term) :: Nil)
+      out must contain(term)
+    }
+
+    "work with FullJoin" in{
+      val out = CoalesceFromClause.extractTerms(FullJoin(term, term) :: Nil)
+      out must contain(term)
     }
   }
 
@@ -32,8 +79,8 @@ class CoalesceFromClauseSpec extends Specification{
 
     "force the right hand side's columns to null" in{
       val nulled = for{
-        (term, table) <- mapped if table.name eq "foo"
-        column <- table.columns if column.name eq "id"
+        (_, table) <- mapped if table.name eq "foo"
+        column <- table.columns
       } yield column
 
       nulled must contain(NullableColumnDef("id", "Int", None))
