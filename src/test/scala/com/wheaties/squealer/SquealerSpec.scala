b9f149b522e9b2dd0b4bc3945f1ae4224d4218dc
Wheaties <owreese@gmail.com>
2012-07-22 16:14:19 -0400

    oops, missed a unit test too

--- src/test/scala/com/wheaties/squealer/SquealerSpec.scala
+++ src/test/scala/com/wheaties/squealer/SquealerSpec.scala
@@ -8,47 +8,11 @@ import definitions._
 import treehuggerDSL._
 import java.sql.DriverManager
 
+//TODO: Look into the Specs2 way of checking on file creation
 class SquealerSpec extends Specification{
   val squeal = new Object with Squealer
-
-  "generateTable" should{
-    val db = Database("foo", Table("foo", None, Column("bar", IntType, None, None, ColumnDef) :: Nil) :: Nil)
-
-    "produce a named table" in{
-      val statement = TableParams("com.wheaties", List("foo"))
-      true must beFalse
-    }
-  }
 
   Class.forName("org.hsqldb.jdbcDriver")
 
   def connection = DriverManager.getConnection("jdbc:hsqldb:mem:aname", "SA", "")
-
-  class SideEffectTester extends Recorder[ParsedResult]{
-    val builder = new StringBuilder
-
-    def record(result: ParsedResult){
-      builder ++= treeToString(result.ast)
-    }
-  }
-
-  "action" should{
-    val con = connection
-    val statement = con.createStatement()
-    statement.execute("CREATE TABLE FOOBAR(id INTEGER, value INTEGER)")
-
-    /*"generate a table when instructed" in{
-      val side = new SideEffectTester
-      squeal.action("test.conf")(side)
-      val out = side.builder.toString
-
-      out must be_==(
-        """import java.sql._
-        object Foobar{
-          def apply(result: ResultSet) = new Foobar(result.geInt("id"), result.getInt("value"))
-        }
-        case class Foobar(id: Int, value: Int)"""
-      )
-    }*/
-  }
 }
